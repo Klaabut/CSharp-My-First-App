51a6daff9fa879b05f534944c1dc90dda91e59d7
Opilane TTHK <opilane@tthk.ee>
2019-09-06 18:01:05 +0300

    commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tere!");
+            Console.WriteLine("Mis su nimi on?");
+            string name;
+
+            name = Console.ReadLine();
+
+            Console.WriteLine("Tere " + name + "!");
+            Console.WriteLine("Mis aastal sa sündisid?");
+            int year;
+            year = int.Parse(Console.ReadLine());
+            int age = 2019 - year;
+            Console.WriteLine("Sa oled " + age + " vana.");
+            Console.ReadLine();
+        }
+    }
+}
